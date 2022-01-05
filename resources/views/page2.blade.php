2d8058cddeffe2ea553b4eae29abf7ea03c29192
IT Helpdesk <helpdesk@tjbservices.com>
2022-01-05 17:21:39 +0700

    upload code

---
+++ resources/views/page2.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riski Atarik</title>
+    <style>
+        body, h1, h2, p, a,td, span {
+            font-family: Arial, Helvetica, sans-serif;
+            text-decoration: none;
+        }
+        .wrap {
+            background-color:rgba(255, 255, 255, 0.7);
+            width: 800px;
+            color:black;
+            margin: 20px auto;
+            padding:15px;
+        }
+        .icon {
+            padding:5px;
+            text-align: center;
+        }
+        span {
+            color: maroon;
+        }
+    </style>
+</head>
+    <div class="wrap" align="center">
+        <h1 align="center">Curriculum <span> Vitae</span></h1>
+        <table>
+            <tr>
+                <td><b>NAMA</b></td><td>:</td> <td>MUHAMAD RISKI ATARIK</td>
+            </tr>
+            <tr>
+                <td><b>TEMPAT, TGL LAHIR</b></td><td>:</td> <td>SEMARANG, 9 MARET 2001</td>
+            </tr>
+            <tr>
+                <td><b>ALAMAT</b></td><td>:</td> <td>SEMARANG</td>
+            </tr>
+            <tr>
+                <td><b>AGAMA</b></td><td>:</td> <td>ISLAM</td>
+            </tr>
+            <tr>
+                <td><b>NOMER TELEPON</b></td><td>:</td> <td>085328814070</td>
+            </tr>
+            <tr>
+                <td><b>Email</b></td><td>:</td><td><a href="mailto:riski.atarik09@gmail.com">riski.atarik09@gmail.com</a></td>
+            </tr>
+        </table>
+    </div>
+    <div class="wrap" align="center">
+        <h2 align="center">PENDIDIKAN</span></h2>
+        <table>
+            <tr>
+                <td><b>2015 - 2018</b></td><td>:</td> <td>SMA N 7 SEMARANG</td>
+            </tr>
+            <tr>
+                <td><b>2019 - 2023</b></td><td>:</td> <td>UNIVERSITAS STIKUBANK SEMARANG</td>
+            </tr>
+        <table>
+
+          <footer>
+                  <ul align="center">
+                      <li><a href="page1">HOME</a></li>
+                      <li><a href="page2">Curriculum Vitae</a></li>
+                      <li><a href="page3">Profil Unisbank</a></li>
+                  </ul>
+          </footer>
+    </div>
+</body>
+</html>
---
+++ resources/views/page3.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riski Atarik</title>
+    <style>
+        body, h1, h2, p, a,td, span {
+            font-family: Arial, Helvetica, sans-serif;
+            text-decoration: none;
+        }
+        .wrap {
+            background-color:rgba(255, 255, 255, 0.7);
+            width: 800px;
+            color:black;
+            margin: 20px auto;
+            padding:15px;
+        }
+        .icon {
+            padding:5px;
+            text-align: center;
+        }
+        span {
+            color: maroon;
+        }
+    </style>
+</head>
+    <div class="wrap" align="center">
+        <h1 align="center">UNIVERSITAS <span> STIKUBANK</span></h1>
+        <table>
+            <tr>
+                <td><b>Universitas Stikubank Semarang, disingkat Unisbank, adalah Entrepreneurial University berbasis teknologi informasi dan komputer yang terletak di pusat kota Semarang.</b></td>
+            </tr>
+        </table>
+    </div>
+    <div class="wrap" align="center">
+
+        <table>
+            <tr>
+                <td><b>Alamat</b></td><td>:</td> <td>Jl. Tri Lomba Juang, Mugassari, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50241</td>
+            </tr>
+            <tr>
+                <td><b>Telepon</b></td><td>:</td> <td>(024) 8451976</td>
+            </tr>
+            <tr>
+                <td><b>Pendaftaran</b></td><td>:</td> <td>5.055 (2014)</td>
+            </tr>
+            <tr>
+                <td><b>Didirikan</b></td><td>:</td> <td>5 Juli 2001, Semarang</td>
+            </tr>
+            <tr>
+                <td><b>Provinsi</b></td><td>:</td> <td>Jawa Tengah</td>
+            </tr>
+            <tr>
+                <td><b>Nama sebelumnya</b></td><td>:</td> <td>Akubank, Stikubank</td>
+            </tr>
+            <tr>
+                <td><b>Rektor</b></td><td>:</td> <td>Dr. Edy Winarno, S.T., M.Eng</td>
+            </tr>
+        <table>
+          <footer>
+                  <ul align="center">
+                      <li><a href="page1">HOME</a></li>
+                      <li><a href="page2">Curriculum Vitae</a></li>
+                      <li><a href="page3">Profil Unisbank</a></li>
+                  </ul>
+          </footer>
+    </div>
+</body>
+</html>
